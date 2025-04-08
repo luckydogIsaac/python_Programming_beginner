9b4a0d10d7a14da421d33b357e37bc16a126843c
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2025-04-08 20:49:15 +0800

    Create ex4-6.py

---
+++ ex4-6.py
@@ -0,0 +1,8 @@
+a = int(input("please input num a:"))
+b = int(input("please input num b:"))
+print("a + b =",a+ b)
+print("a - b =",a+ b)
+print("a + b =",a+ b,"a - b =", a - b, a * b, a / b)
+
+x, y = 10, 3
+print(x + y, x - y, x * y, x / y, x % y, x ** y, x //y)
